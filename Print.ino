761697b6115ce53ea3dd2dd7eb716b96e7562c8b
TheAmo <apalaosds@gmail.com>
2016-10-26 17:02:29 -0400

    Update Print.ino

--- Print.ino
+++ Print.ino
@@ -3,10 +3,16 @@ void setup(){
 }
 
 void loop(){
-  for(int i = 4; i >= 0; i--){
-    Serial.print(" ");
-  }
-  for(int i = 0; i <= 4; i++){
-    Serial.print("*");
+  int k = 4;
+  int h = 0;
+  for(int g = 0; g <= 2; g++){
+    for(int i = 4; i >= h; i--){
+      Serial.print(" ");
+    }
+    for(int i = 0; i <= k; i++){
+      Serial.print("*");
+    }
+    k--;
+    h++;
   }
 }
